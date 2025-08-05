27d899b10db206aadbece5cb70c133832690989b
Farhan Nadim <farhannadim2023@gmail.com>
2025-08-05 22:59:34 +0600

    Project set up-dine

---
+++ Amar_Dukan/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Amar_Dukan project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Amar_Dukan.settings')
+
+application = get_asgi_application()
