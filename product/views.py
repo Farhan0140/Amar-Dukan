78ee701c8f69025cf94afe0eda3f874e73cd51af
Farhan Nadim <farhannadim2023@gmail.com>
2025-08-09 10:46:58 +0600

    Refactor serializers and views to remove commented-out code, improving readability and maintainability.

--- product/views.py
+++ product/views.py
@@ -25,28 +25,6 @@ def view_products( request ):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-# @api_view()
-# def view_specific_product( request, id ):
-#     try:
-#         product = Product.objects.get(pk = id)
-#         product_dict = {
-#             'id': product.id,
-#             'name': product.name,
-#             'price': product.price,
-#             'stock': product.stock,
-#             'description': product.description,
-#         }
-#     except Product.DoesNotExist:
-#         return Response(
-#             {'message': 'Product Does Not Exist'},
-#             # status = 404,     # not recommended
-#             status = status.HTTP_404_NOT_FOUND,
-#         )
-
-#     return Response( product_dict )
-
-# ---------> Simple Version
-
 @api_view(['GET', 'PUT', 'DELETE'])
 def view_specific_product( request, pk ):
     product = get_object_or_404( Product, pk = pk )
