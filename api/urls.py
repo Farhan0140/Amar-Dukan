ab1183708c0de148068ea0d69f4b47e3cb2fc184
Farhan Nadim <farhannadim2023@gmail.com>
2025-08-09 17:55:52 +0600

    Refactor API routing to use DefaultRouter for improved structure and functionality.

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,19 @@
 
 from django.urls import path, include
 
-from rest_framework.routers import SimpleRouter
+from rest_framework.routers import DefaultRouter
 
 from product.views import Product_View_Set, Category_View_Set
 
 
-router = SimpleRouter()
+router = DefaultRouter()
 router.register('products', Product_View_Set)
 router.register('categories', Category_View_Set)
 
-urlpatterns = router.urls
+# urlpatterns = router.urls
+
+urlpatterns = [
+    path('', include(router.urls)),
+    # path('something/' , )
+]
+
