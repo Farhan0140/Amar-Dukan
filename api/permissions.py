f94d0d817f5eaee3896766c4ec893c9c513e4e03
Farhan Nadim <farhannadim2023@gmail.com>
2025-08-16 11:50:28 +0600

    Permissions & Custom Permission Class ->> Implement custom permissions and update views for admin access control

---
+++ api/permissions.py
@@ -0,0 +1,11 @@
+
+from rest_framework import permissions
+
+
+class IsAdmin_Or_ReadOnly( permissions.BasePermission ):
+    def has_permission(self, request, view):
+        # if request.method == 'GET':
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
+        return bool(request.user and request.user.is_staff)
